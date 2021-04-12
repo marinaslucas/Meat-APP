d1aa912b2ae3d47d8d6c0dcf318c4cfaa0d5f442
Marina Lucas <marinarlucas@gmail.com>
2021-04-11 21:55:37 -0300

    Commit Parcial Course - Restaurant details

--- meat-app-starter/src/app/restaurants/restaurant.service.ts
+++ meat-app-starter/src/app/restaurants/restaurant.service.ts
@@ -1,12 +1,15 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Injectable} from '@angular/core'
+import {Http} from '@angular/http'
 
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
-import { Restaurant } from "./restaurant/restaurant.model";
 
-import {MEAT_API} from '../app.api';
+import { Restaurant } from "./restaurant/restaurant.model"
+
+import {MEAT_API} from '../app.api'
+import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()//serviço recebendo dados de um outro serviço, devo marcar com injectable
 export class RestaurantsService {
@@ -16,8 +19,13 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]> { // O método restaurants vai me retornar um tipo observable e dentro do observable vai o tipo de dado, que é o model Restaurant. Portanto, estou tipando o tipo de dado que será retornado. Poderia por ex. sem tipagem, aí ficaria Observable <any>
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
-        
+        restaurantById(id: string): Observable<Restaurant>{
+            return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+        }
     }
 
 
